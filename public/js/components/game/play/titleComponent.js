06df36d90e86b1c8cd3c244a6b08c5ad49d2dd28
javier.martinez <jma96.elx@gmail.com>
2021-02-13 01:56:42 +0100

    Retoques title component

--- public/js/components/game/play/titleComponent.js
+++ public/js/components/game/play/titleComponent.js
@@ -1,10 +1,16 @@
 let titleComponent = Vue.component("title-component", {
   props: ["ganador", "host", "code", "serverInfo"],
+  data: function () {
+    return {
+        msgCabecera: "Encuentra la palabra oculta!",
+    }
+},
   template:
     `<div>
-        <h2 class="row p-2 align-self-center justify-content-center" v-if="this.serverInfo.estado === 0">{{ this.host }} - Partida: {{this.code}}</h2>
+      <h2 class="row p-2 align-self-center justify-content-center" v-if="this.serverInfo.estado === 0">{{ this.host }} - Partida: {{this.code}}</h2>
+        <h3 class="row p-2 align-self-center justify-content-center" v-if="this.serverInfo.estado === 1">{{ this.msgCabecera}}</h3>
         <h2 class="row p-2 align-self-center justify-content-center" v-if="this.serverInfo.estado === 1">Ronda: {{ this.serverInfo.ronda}}/{{ this.serverInfo.maxRondas}} - &nbsp;
-        <button type="button" class="btn btn-danger col-2">Tiempo <span class="badge badge-light bg-light text-dark"> {{this.serverInfo.tiempo}}</span></button></h2>
+        <span class="badge bg-danger col-12 col-md-4 col-lg-2"> Tiempo: {{this.serverInfo.tiempo}}</span></h2>
         
         <template v-if="this.serverInfo.estado === 2 ">
           <h2 v-if="this.ganador.length === 1" class="row p-2 align-self-center justify-content-center" >
